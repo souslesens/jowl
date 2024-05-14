027d1f07e27defb977d143b0963351eedba75b85
Nasreddine Bouchemel <bouchemelnasreddine@gmail.com>
2024-05-14 13:00:44 +0200

    fix dotenv import issue

--- src/main/java/com/souslesens/Jowl/JowlApplication.java
+++ src/main/java/com/souslesens/Jowl/JowlApplication.java
@@ -1,15 +1,10 @@
 package com.souslesens.Jowl;
 
-import com.souslesens.Jowl.config.AppConfig;
-import io.github.cdimascio.dotenv.Dotenv;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
-
-import java.util.Arrays;
 
 @SpringBootApplication
 public class JowlApplication {
@@ -23,11 +18,6 @@ public class JowlApplication {
 	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
 		return args -> {
 		};
-	}
-
-	private static void loadEnv() {
-		Dotenv dotenv = Dotenv.configure().load();
-
 	}
 
 }
